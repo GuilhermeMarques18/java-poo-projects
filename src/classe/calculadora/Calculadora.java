9c521529081209e5a5ad2dd66666a8a2e2bfff36
Guilherme Marques <mr.gmrsouza@gmail.com>
2026-04-14 21:16:11 -0300

    feat: activity calculator completed

--- src/classe/calculadora/Calculadora.java
+++ src/classe/calculadora/Calculadora.java
@@ -1,4 +1,53 @@
 package classe.calculadora;
 
-public class Calculadora {
+public class Calculadora{
+    private double display;
+    private int battery;
+    private int batteryMax;
+
+
+    public Calculadora( int batteryMax){
+        this.display=0.0;
+        this.battery=0;
+        this.batteryMax = batteryMax;
+
+    }
+
+
+    public void recarregar(int valor){
+        this.battery+=valor;
+        if(this.battery >batteryMax){
+            this.battery=batteryMax;
+        }
+
+    }
+
+    public void somar(int a, int b){
+        if(battery==0){
+            System.out.println("fail: bateria insuficiente");
+            return;
+        }
+        display=a+b;
+        battery --;
+    }
+
+    public void divisao(double a, double b){
+        if(battery<=0){
+            System.out.println("fail: bateria insuficiente");
+            return;
+        }
+        if(b==0){
+            System.out.println("fail: divisao por zero");
+            battery --;
+            return;
+        }
+        display= a/b;
+        battery --;
+    }
+
+    public String  toString(){
+        return String.format("display = %.2f, battery = %d", display, battery);
+
+    }
+
 }
--- src/classe/calculadora/Solver.java
+++ src/classe/calculadora/Solver.java
@@ -1,4 +1,49 @@
 package classe.calculadora;
 
-public class Solver {
+import java.util.*;
+import java.text.DecimalFormat;
+
+public class Solver{
+
+    public static void main(String[] args){
+        Calculadora calculo = null;
+        Scanner scanner = new Scanner(System.in);
+
+        while(true){
+            var line=scanner.nextLine();
+            System.out.println("$" + line);
+
+            var par = line.split(" ");
+            var cmd = par[0];
+
+            if(cmd.equals("end")){
+                break;
+            }
+            else if (cmd.equals("init")){
+                int max=Integer.parseInt(par[1]);
+                calculo=new Calculadora(max);
+            }
+            else if(cmd.equals("show")){
+                System.out.println(calculo);
+            }
+            else if(cmd.equals("charge")){
+                int valor = Integer.parseInt(par[1]);
+                calculo.recarregar(valor);
+            }
+            else if(cmd.equals("sum")){
+                int a=Integer.parseInt(par[1]);
+                int b=Integer.parseInt(par[2]);
+                calculo.somar(a, b);
+            }
+            else if(cmd.equals("div")){
+                double num = Double.parseDouble(par[1]);
+                double den = Double.parseDouble(par[2]);
+                calculo.divisao(num,den);
+            }
+            else {
+                System.out.println("fail: comando invalido");
+
+            }
+        }
+    }
 }
